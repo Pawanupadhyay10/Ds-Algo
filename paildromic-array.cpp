e2350d259f64c1462581e569772b2bc99b1db4b9
pawan10 <pawandev1099@gmail.com>
2021-05-02 16:05:27 +0530

    palindromic array

---
+++ paildromic-array.cpp
@@ -0,0 +1,16 @@
+int PalinArray(int a[], int n)
+{  //add code here.
+for(int i=0;i<n;i++ ){
+    int ans=0;
+    int temp=a[i];
+    while(temp>0){
+        int r=temp%10;
+        temp/=10;
+        ans=ans*10+r;
+    }
+    if(ans!=a[i]){
+       return 0; 
+    }
+    return 1;
+}
+}
