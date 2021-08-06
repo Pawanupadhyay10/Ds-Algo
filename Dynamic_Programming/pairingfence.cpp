034307ccd33ba6eda7e2cae6ef385cad9f205f35
pawan10 <pawandev1099@gmail.com>
2021-08-06 21:41:19 +0530

    Pairing fence

---
+++ Dynamic_Programming/pairingfence.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    long long countWays(int n, int k)
+    {
+        // code here
+        long dp[n + 1];
+        memset(dp, 0, sizeof(dp));
+        long long mod = 1000000007;
+
+        dp[1] = k;
+        dp[2] = k * k;
+
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = ((k - 1) * (dp[i - 1] + dp[i - 2])) % mod;
+        }
+
+        return dp[n];
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        Solution ob;
+        cout << ob.countWays(n, k) << endl;
+    }
+    return 0;
+} // } Driver Code Ends
